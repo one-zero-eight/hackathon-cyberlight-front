bfd4c2091f41c17bc9bc7eae271d4bef9bcbe70d
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2023-12-06 22:59:43 +0300

    chore: initial structure for next.js project

---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="ru">
+      <Head />
+      <body>
+      <noscript className="flex w-full justify-center bg-red-700 p-8">
+        Включите JavaScript в браузере для корректной работы страницы.
+      </noscript>
+      <Main />
+      <NextScript />
+      </body>
+    </Html>
+  );
+}
