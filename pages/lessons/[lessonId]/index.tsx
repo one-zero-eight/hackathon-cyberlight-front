8511eb87c2641741edd88698db12ec2fba00ff9f
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2023-12-08 18:32:48 +0300

    chore: redirect to first task

--- pages/lessons/[lessonId]/index.tsx
+++ pages/lessons/[lessonId]/index.tsx
@@ -3,6 +3,7 @@ import { Lesson } from "@/lib/lesson";
 import { Container } from "@mantine/core";
 import { useQuery } from "@tanstack/react-query";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function Page() {
   const router = useRouter();
@@ -14,6 +15,14 @@ export default function Page() {
     queryKey: [`/lessons/${lessonId}`],
     enabled: lessonId !== undefined,
   });
+
+  useEffect(() => {
+    if (lesson === undefined) {
+      return;
+    }
+
+    router.push(`/lessons/${lesson.id}/0`);
+  }, [lesson, router]);
 
   return (
     <Layout>
