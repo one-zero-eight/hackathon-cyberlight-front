c630c4a73ab291fdc0d78b72179aa7728a5b8c7a
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2023-12-08 17:18:50 +0300

    chore: add lessons list

---
+++ pages/lessons/index.tsx
@@ -0,0 +1,24 @@
+import Layout from "@/components/Layout";
+import { Lesson } from "@/lib/lesson";
+import { Container, Paper } from "@mantine/core";
+import { useQuery } from "@tanstack/react-query";
+import Link from "next/link";
+
+export default function Page() {
+  const { data: lessons } = useQuery<Lesson[]>({
+    queryKey: ["/lessons/"],
+  });
+
+  return (
+    <Layout>
+      <Container className="flex flex-col gap-2">
+        <h1 className="text-2xl">Уроки</h1>
+        {lessons?.map((lesson) => (
+          <Paper key={lesson.id}>
+            <Link href={`/lessons/${lesson.id}`}>Урок</Link>
+          </Paper>
+        ))}
+      </Container>
+    </Layout>
+  );
+}
