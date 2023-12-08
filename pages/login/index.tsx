929b036b6eb884ea1d887b5e01b75165b0cf0d39
evermake <vladeryabkin@gmail.com>
2023-12-08 17:07:44 +0300

    fix: improve login page

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -6,17 +6,17 @@ import Layout from "@/components/Layout";
 export default function Page() {
   const form = useForm({
     initialValues: {
-      email: "",
+      login: "",
       password: "",
     },
     validate: {
-      email: (val) => {
-        if (!val.includes("@")) {
-          return "Email должен содержать @";
+      login: (val) => {
+        if (val.length < 3) {
+          return "Логин должен быть длиннее 3 символов";
         }
       },
       password: (val) => {
-        if (val.length < 6) {
+        if (val.length < 4) {
           return "Пароль должен быть длиннее 4 символов";
         }
       },
@@ -29,24 +29,26 @@ export default function Page() {
       <Paper withBorder shadow="sm" className="w-full max-w-[300px] p-4">
         <form
           className="flex flex-col gap-4"
-          onSubmit={form.onSubmit(({ email, password }) =>
-            signIn.mutate({ login: email, password }),
+          onSubmit={form.onSubmit(({ login, password }) =>
+            signIn.mutate({ login: login, password }),
           )}
         >
           <h1 className="text-center text-2xl font-medium">Вход</h1>
-          {signIn.isError && (
-            <p className="text-red-500">Неверный логин или пароль.</p>
-          )}
           <TextInput
             size="md"
-            placeholder="E-mail"
-            {...form.getInputProps("email")}
+            placeholder="Логин"
+            type="text"
+            {...form.getInputProps("login")}
           />
           <TextInput
             size="md"
             placeholder="Пароль"
+            type="password"
             {...form.getInputProps("password")}
           />
+          {signIn.isError && (
+            <p className="text-red-500">Неверный логин или пароль.</p>
+          )}
           <Button type="submit">Войти</Button>
         </form>
       </Paper>
