7237b4578116c1dc90b7049dc0b6e90daee7b745
evermake <vladeryabkin@gmail.com>
2023-12-09 17:51:31 +0300

    feat: improve leaderboard styling

--- pages/profile/leaderboard.tsx
+++ pages/profile/leaderboard.tsx
@@ -1,5 +1,6 @@
 import Layout from "@/components/Layout";
-import { Container, Paper, Title } from "@mantine/core";
+import Section from "@/components/Section";
+import { Container, Paper, Table, Title } from "@mantine/core";
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
 
@@ -10,23 +11,46 @@ export type LeaderboardRecord = {
 };
 
 export default function Page() {
-  const { data: leaderboard } = useQuery<LeaderboardRecord[]>({
+  const { data: leaders } = useQuery<LeaderboardRecord[]>({
     queryKey: ["/personal_account/leaderboard"],
   });
+
+  const rows =
+    leaders?.map((leader, i) => (
+      <Table.Tr key={i}>
+        <Table.Td>
+          {i === 0 && (
+            <span className="icon-[mdi--medal-outline] align-middle text-[24px] text-[#ffd700]"></span>
+          )}
+          {i === 1 && (
+            <span className="icon-[mdi--medal-outline] align-middle text-[24px] text-[#c0c0c0]"></span>
+          )}
+          {i === 2 && (
+            <span className="text-m icon-[mdi--medal-outline] align-middle text-[24px] text-[#cd7fr2]"></span>
+          )}
+        </Table.Td>
+        <Table.Td>{i + 1}</Table.Td>
+        <Table.Td>{leader.name || "Аноним"}</Table.Td>
+        <Table.Td>{leader.total_exp}</Table.Td>
+      </Table.Tr>
+    )) ?? [];
+
   return (
     <Layout>
-      <Container className="flex flex-col">
-        <Title order={1}>Рейтинг</Title>
-        <Paper className="flex flex-col p-8" shadow="md">
-          {leaderboard?.map((record) => (
-            <div key={record.id} className="flex flex-row items-center gap-2">
-              <span className="icon-[mdi--trophy] text-yellow-900" />
-              <p>
-                {record.name || "Аноним"} - {record.total_exp} xp
-              </p>
-            </div>
-          ))}
-        </Paper>
+      <Container>
+        <Section title="Рейтинг">
+          <Table className="m-auto max-w-[500px]">
+            <Table.Thead>
+              <Table.Tr>
+                <Table.Th></Table.Th>
+                <Table.Th>Рейтинг</Table.Th>
+                <Table.Th>Имя</Table.Th>
+                <Table.Th>Опыт</Table.Th>
+              </Table.Tr>
+            </Table.Thead>
+            <Table.Tbody>{rows}</Table.Tbody>
+          </Table>
+        </Section>
       </Container>
     </Layout>
   );
